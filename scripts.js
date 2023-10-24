2183872eeafb383e6ebc50fd4e16699b24ef0695
John Dungao <johnmiguel.dungao@gmail.com>
2023-10-24 16:37:07 -0400

    add fetch, and finish logic

--- scripts.js
+++ scripts.js
@@ -1,63 +1,112 @@
-const freelancers = [
-  { name: "Dr. Slice", price: 25, occupation: "gardener" },
-  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-  { name: "Prof. Prism", price: 81, occupation: "teacher" },
-  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-  { name: "Prof. Spark", price: 76, occupation: "programmer" },
-  { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" },
-];
+async function fetchData() {
 
-//container
-const contractorInfo = document.querySelector(".contractors-list")
+    const apiURL = "https://api.npoint.io/2ecaba192fe5cc525747";
 
-//initial State
-const initialState = (function(){
-    let initialVal = [];
-    for(let i = 0; i <= 1; i++) {
-        initialVal.push(freelancers.shift());
+    try {
+        const res = await fetch(apiURL);
+        if (!res.ok) {
+            throw new Error('Network response was error')
+        }
+        const data = await res.json();
+        return data;
+    } catch(error) {
+        console.error("Problem with fetch");
     }
-    return initialVal;
-})();
-
-
-// Functions
-const getAverage = (arr) => {freelancers.map(freelancer => freelancer.price).reduce((acc, currentVal) => acc + currentVal, 0) / arr.length;}
-const capetalize = (str) => {
-    //first letter
-    const fLetter = str.substring(0,1).toUpperCase();
-    //remaining string
-    let restLetter = str.slice(1, str.length);
-    return fLetter + restLetter;
 }
 
+async function main () {
+    const freelancers = await fetchData();
+    const contractorInfo = document.querySelector(".contractors-list")
+    //Initial State
+    let initialState = freelancers[0];
+
+    //initial State
+    if (freelancers) {
+        console.log(freelancers);
+        initialState = (()=>{
+        let initialVal = [];
+        for(let i = 0; i <= 1; i++) {
+            initialVal.push(freelancers.shift());
+        }
+        return initialVal;
+    })();
+    }
 
 
-const displayFreelancers = (listOfFreelancers) => {
-    listOfFreelancers.forEach(freelancer => {
-        const {name, price, occupation} = freelancer;
-        // Container
-        const container = document.createElement('div');
-        container.className = "contractor-info";
 
-        //Contractor Info
-        const cName = document.createElement('p')
-        cName.textContent = name;
-        const cPrice = document.createElement('p');
-        cPrice.textContent = `$${price}`;
-        const cOccupation = document.createElement('p')
-        cOccupation.textContent = capetalize(occupation);
+    // Functions
+    const getAverage = () => {
+        const parentContainer = document.querySelector('#title');
+        const avgElem = document.querySelector("#avg-price")
+        const priceList = document.querySelectorAll(".contractor-price");
+        const prices = [...priceList];
 
-        // Append Contractor info to container
-        container.appendChild(cName)
-        container.appendChild(cPrice)
-        container.appendChild(cOccupation)
+        let avg = (prices
+            .map(price => Number(price.textContent.substring(1, price.textContent.length)))
+            .reduce((acc, currVal) => acc + currVal, 0)) /prices.length;
 
-        //Append to the parent container
-        contractorInfo.appendChild(container);
-        
-    })
+        if (avgElem) {
+            avgElem.textContent = Math.round(avg);
+        } else {
+            const avgElem = document.createElement('h3');
+            avgElem.textContent = Math.round(avg);
+            avgElem.id = 'avg-price'
+            parentContainer.appendChild(avgElem)
+        }
+    
+    }
+
+
+    const capitalize = (str) => {
+        const chars = [...str];
+        chars[0] = chars[0].toUpperCase();
+        return str = chars.join("")
+    }
+    const createContractorElem = ({name, price, occupation}) => {
+            // Container
+            const container = document.createElement('div');
+            container.className = "contractor-info";
+
+            //Contractor Info
+            const cName = document.createElement('p')
+            cName.className = 'contractor-name'
+            cName.textContent = name;
+            const cPrice = document.createElement('p');
+            cPrice.className = 'contractor-price'
+            cPrice.textContent = `$${price}`;
+            const cOccupation = document.createElement('p')
+            cOccupation.className = 'contractor-occupation'
+            cOccupation.textContent = capitalize(occupation);
+
+            // Append Contractor info to container
+            container.appendChild(cName)
+            container.appendChild(cPrice)
+            container.appendChild(cOccupation)
+            
+            contractorInfo.append(container)
+            //Append to the parent container\
+            
+    }
+    const appendToDOM = (arr) => {
+        if(arr.length > 2) {
+            arr.forEach((freelancer, i ) => {
+                setTimeout(() => {
+                    createContractorElem(freelancer)
+                    getAverage()
+                },i * 1500)
+            })
+        } else {
+            arr.forEach(freelancer => createContractorElem(freelancer))
+        }
+    
+    }
+
+
+
+
+    appendToDOM(initialState);
+    appendToDOM(freelancers);
+
 }
 
-displayFreelancers(initialState);
+main();
